8a50764311984e1537f6fd0d667a66a7cd8f2b0c
ESSAKHI10 <79039587+ESSAKHI10@users.noreply.github.com>
2022-08-09 13:48:57 +0100

    add crud service for course

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,26 +1,34 @@
+import { Course } from 'src/course/entities/course.entity';
 import { Injectable } from '@nestjs/common';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CourseService {
+  constructor(
+    @InjectRepository(Course)
+    private CourseRepository: Repository<Course>,
+  ) {}
+
   create(createCourseDto: CreateCourseDto) {
-    return 'This action adds a new course';
+    return this.CourseRepository.save(createCourseDto);
   }
 
   findAll() {
-    return `This action   returns all course`;
+    return this.CourseRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} course`;
+    return this.CourseRepository.findOne(id);
   }
 
   update(id: number, updateCourseDto: UpdateCourseDto) {
-    return `This action updates a #${id} course`;
+    return this.CourseRepository.update(id, updateCourseDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} course`;
+    return this.CourseRepository.delete(id);
   }
 }
